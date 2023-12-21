4b99804682f8c6dcce2d5c3c01e1bdf9a393bbe9
bonheurbaleks <bonheurbalekoua@gmail.com>
2023-12-21 21:52:15 +0300

    finished the model and updated the project

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,19 +1,30 @@
 "use client"
 
 import Link from "next/link"
-import React from "react"
+import React, { useEffect }  from "react"
 import { useRouter } from "next/navigation"
 import { axios } from "axios"
 export default function SignupPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: ""
   })
+  const [buttonDisabled, setButtonDisabled] = React.useState(false)
 
   const onSignup = async () => {
 
   }
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true)
+    }
+
+  }, [user])
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -50,7 +61,7 @@ export default function SignupPage() {
      />
      <button
      onClick={onSignup}
-      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outliine-none focus:border-gray-600 ">Signup here</button>
+      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outliine-none focus:border-gray-600 ">{buttonDisabled ? "No signup" : "Sign up"}</button>
       <Link href="/login">Visit Login page</Link>
           </div>
   )
---
+++ src/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Pleaseprovide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+})
+
+// IF there's a user add, if not create
+
+const User = mongoose.models.Users || mongoose.model("users", userSchema)
+
+export default User
