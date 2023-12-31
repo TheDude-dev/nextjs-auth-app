27fb903323cdfeb11f21e337249673816273ddcf
bonheurbaleks <bonheurbalekoua@gmail.com>
2023-12-31 17:09:36 +0300

    feat: add token

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/dbconfig/dbConfig"
 import User from "@/models/userModel"
 import { NextRequest, NextResponse  } from "next/server"
 import bcryptjs from "bcryptjs"
-
+import jwt from "jsonwebtoken"
 
 connect()
 
@@ -24,7 +24,30 @@ export async function POST(request: NextRequest) {
        const validPassword = await bcryptjs.compare
        (password, user.password)
 
+       if(!validPassword) {
+        return NextResponse.json({error: "Invalid password"}, {status: 400})
+       }
 
+       // Create token data
+       const tokenData = {
+        id: user._id,
+        username: user.username,
+        email: user.email
+       }
+
+       // create token
+       const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1d"})
+
+       const response = NextResponse.json({
+        message: "Login successful",
+        success: true,  
+    })
+
+    response.cookies.set("token", token, {
+        httpOnly: true,
+    }) 
+
+    return response
 
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
