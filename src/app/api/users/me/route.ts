eb77804c7f3c73f320ff84278910dec7e18d661b
bonheurbaleks <bonheurbalekoua@gmail.com>
2024-01-13 20:18:33 +0300

    feat: added GetUser details button

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -6,9 +6,15 @@ connect()
 
 export async function GET(request: NextRequest) {
     try{
+        const userId = await getDataFromToken(request)
+        const user = await User.findOne({_id: userId}).select("-password")
+        return NextResponse.json({
+            message: "User found",
+            data: user
+        })
         
     } catch(error: any) {
-        return NextResponse.json({error: error.message}, {status: 500})
+        return NextResponse.json({error: error.message}, {status: 400})
     }
 }
 
